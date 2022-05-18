403890af55230b6644f9007a73211c7bb45cfdbd
Prakash <prakashmandal.iv@gmail.com>
2022-05-18 19:02:47 +0530

    Add : regestration function

--- Script/script.js
+++ Script/script.js
@@ -4,65 +4,78 @@ const Loginurl = 'http://localhost:5003/Controller/login'
 const RegUrl = 'http://localhost:5003/Controller/register-user'
 //Login Page script
 const loginform = document.getElementById('loginform')
-loginform.addEventListener('submit', function (e){
+loginform.addEventListener('submit', function (e) {
     e.preventDefault();
 
     username = document.getElementById('username').value
     password = document.getElementById('password').value
-    
-    data = {
+
+    logindata = {
         userName: `${username}`,
         password: `${password}`
     }
-    detail = JSON.stringify(data)
-    console.log(detail)
-    fetch(Loginurl,{
+    fetch(Loginurl, {
         method: 'POST',
-        headers:{
+        headers: {
             "Access-Control-Allow-Headers": "*",
             "Access-Control-Allow-Methods": 'OPTIONS,POST,GET',
             'Content-Type': 'application/json; charset=UTF-8'
         },
-        body: JSON.stringify(data)
-    }).then(function (response){
-        if(response.status == 400)
-        {
+        body: JSON.stringify(logindata)
+    }).then(function (response) {
+        if (response.status == 400) {
             document.getElementById('username').style.border = "1px solid red"
             document.getElementById('password').style.border = "1px solid red"
         }
         else {
-            token = response.text()         
-        }
-        return response.text();     
-    }).then(function (text){
-          
+            token = response.text()
+        }   
+    }).then(function (text) {
+
     })
 })
 
 //login registration toggle button
-function OpenReg()
-{
-    document.getElementById('logpanel').style.display ="none"
-    document.getElementById('regpanel').style.display ="block"
+function OpenReg() {
+    document.getElementById('logpanel').style.display = "none"
+    document.getElementById('regpanel').style.display = "block"
 }
-function OpenLogin()
-{
-    document.getElementById('regpanel').style.display ="none"
-    document.getElementById('logpanel').style.display ="block"
+function OpenLogin() {
+    document.getElementById('regpanel').style.display = "none"
+    document.getElementById('logpanel').style.display = "block"
 }
 
 //registration script
 const RegForn = document.getElementById('registrationform')
-RegForn.addEventListener('submit',function (e){
+RegForn.addEventListener('submit', function (e) {
     e.preventDefault()
     username = document.getElementById('regusername').value
     firstname = document.getElementById('regfirstname').value
     lastname = document.getElementById('reglastname').value
     password = document.getElementById('regpassword').value
-    data = {
+    regdata = {
         "username": `${username}`,
         "firstName": `${firstname}`,
         "lastName": `${lastname}`,
         "password": `${password}`
-      }
+    }  
+    fetch(RegUrl, {
+        method: 'POST',
+        headers: {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": 'OPTIONS,POST,GET',
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify(regdata)
+    }).then(function (response){
+        if(response.status == 200)
+        {
+            alert("Successfully Registers , please login!")
+            document.getElementById('regpanel').style.display = "none"
+            document.getElementById('logpanel').style.display = "block"
+        }
+        return response.text();
+    })
 })
+
+//Main File
